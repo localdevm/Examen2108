d9e85035df54dafbe184a5fbca8a5c3409128432
Maarten <maartenwachtersmast@gmail.com>
2017-08-21 15:11:23 +0200

    email uitbreding

--- app/src/main/java/com/example/laptopmaarten/examenmaarten2108/MainActivity.java
+++ app/src/main/java/com/example/laptopmaarten/examenmaarten2108/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.laptopmaarten.examenmaarten2108;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,9 +15,18 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+/**********************************************
+ * GEDANE UITBREIDINGEN
+ * - E-mail button
+ * - Notification
+ * - Fouten opslagen in preferences (werkt niet helemaal)
+ *
+ */
 public class MainActivity extends AppCompatActivity {
     int getal1, getal2, getal3;
     public int fout;
+    Context context = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         int def = getResources().getInteger(R.string_.saved_fout_default);
         int fout = preferences.getInt(getString(R.string.saved_fout), def);
         */
+
         final Intent wallet = new Intent(getApplicationContext(), wallet.class);
 
         final TextView txtGetal1 = (TextView) findViewById(R.id.txtgetal1);
@@ -38,7 +52,10 @@ public class MainActivity extends AppCompatActivity {
         Button button2m = (Button) findViewById(R.id.btn2min);
         Button button3p = (Button) findViewById(R.id.btn3plus);
         Button button3m = (Button) findViewById(R.id.btn3min);
+        final Button recover = (Button) findViewById(R.id.btnrecover);
         Button buttonCheck = (Button) findViewById(R.id.btncheck);
+
+
 
        button1p.setOnClickListener(new View.OnClickListener() {
            @Override
@@ -99,6 +116,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        recover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                        "mailto","kristof.overdulve@gmail.com", null));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Forgot vault code");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "I forgot my vault code. Please send it by email");
+                startActivity(Intent.createChooser(emailIntent, "Send email..."));
+            }
+        });
+
         buttonCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -118,7 +146,22 @@ public class MainActivity extends AppCompatActivity {
                     txtGetal2.setText("" + getal2);
                     getal3 = 0;
                     txtGetal3.setText("" + getal3);
+                    recover.setVisibility(View.VISIBLE);
+                    if (fout >= 5)
+                        sendNotification(v);
                 }
+            }
+
+            //Notification uitbreding
+            public void sendNotification(View v){
+                NotificationCompat.Builder mBuilder =
+                        (NotificationCompat.Builder) new NotificationCompat.Builder(context)
+                                .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                                .setContentTitle("Intruder Alert")
+                                .setContentText("We suspect malious behaviour on your account and have notified you via email");
+
+                NotificationManager mnotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                mnotificationManager.notify(001, mBuilder.build());
             }
         });
 
